027a26076011258b75086dd28bfee826a113c090
ellyheetov <yaaajcm@naver.com>
2020-10-17 21:15:37 +0900

    feat: make updateBoard method

--- src/main/java/org/diary4us/service/impl/BoardServiceImpl.java
+++ src/main/java/org/diary4us/service/impl/BoardServiceImpl.java
@@ -17,25 +17,36 @@ public class BoardServiceImpl implements BoardService {
     BoardInformDao boardInformDao;
 
     @Override
+    public BoardInform getBoard(Long id) {
+        BoardInform boardInform = boardInformDao.selectById(id);
+        return boardInform;
+    }
+
+    @Override
     public List<BoardInform> getBoards(Integer start) {
-        List<BoardInform> list = boardInformDao.selectAll(start, BoardService.LIMIT);
+        List<BoardInform> list = boardInformDao.selectSome(start, BoardService.LIMIT);
         return list;
     }
 
     @Override
-    @Transactional(readOnly = false)
+    @Transactional
     public int deleteBoard(Long id) {
         int deleteCount = boardInformDao.deleteById(id);
         return deleteCount;
     }
 
     @Override
-    @Transactional(readOnly = false)
-    public BoardInform addBoard(BoardInform boardInform) {
+    @Transactional
+    public BoardInform insertBoard(BoardInform boardInform) {
         boardInform.setRegdate(new Date());
         Long id = boardInformDao.insert(boardInform);
-//        boardInform.setBoardId(id);
+        boardInform.setBoardId(id);
+        return boardInform;
+    }
 
+    @Override
+    public BoardInform updateBoard(BoardInform boardInform) {
+        boardInformDao.update(boardInform);
         return boardInform;
     }
 
