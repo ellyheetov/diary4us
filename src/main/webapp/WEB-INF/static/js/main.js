a2b9344968908d3e9a8c7f9d734751e851020847
justinjeong <justin.jeong5@gmail.com>
2020-10-29 20:00:08 +0900

    기본구조 선언

--- src/main/webapp/WEB-INF/static/js/main.js
+++ src/main/webapp/WEB-INF/static/js/main.js
@@ -1,3 +1,15 @@
-const refresh = () => {
-    location.reload();
+const getHTML = (url) => {
+  fetch(url).then((resolve) => {
+    return resolve.json();
+  }).catch((error) => {
+    console.log(error);
+  })
+}
+
+const insertHTML = (url, target) => {
+  const base = document.querySelector('.' + target);
+  const page = getHTML(url);
+
+  base.appendChild(page);
+
 }
